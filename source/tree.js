147a841d057c92e19a11c85d0cabf3caaeac52f8
BorodinDmitriy <demansen@yandex.ru>
2018-02-06 20:04:05 +0300

    const fix + camelCase

--- source/tree.js
+++ source/tree.js
@@ -1,18 +1,18 @@
 'use strict'
 
-let tree = number => {
+const tree = number => {
 	if (number < 3) {
 		return null;
 	} 
 
-	let space_repeater = number - 2;
+	let spaceRepeater = number - 2;
 	let result = "";
-	const init_space_repeater = space_repeater;
+	const initSpaceRepeater = spaceRepeater;
 
-	for (let star_repeater = 0; star_repeater < number - 1; star_repeater++, space_repeater--) {
-		result += " ".repeat(space_repeater) + "*".repeat(2 * star_repeater + 1) + " ".repeat(space_repeater) + "\n";
+	for (let starRepeater = 0; starRepeater < number - 1; starRepeater++, spaceRepeater--) {
+		result += " ".repeat(spaceRepeater) + "*".repeat(2 * starRepeater + 1) + " ".repeat(spaceRepeater) + "\n";
 	}
-	result += " ".repeat(init_space_repeater) + "|" + " ".repeat(init_space_repeater) + "\n";
+	result += " ".repeat(initSpaceRepeater) + "|" + " ".repeat(initSpaceRepeater) + "\n";
 	
 	return result;
 };
